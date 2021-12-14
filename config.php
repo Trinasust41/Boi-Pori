3735c02895842dab81320dd0c79d75005f497adb
Trina Chakraborty <48939847+Trinasust41@users.noreply.github.com>
2021-12-14 11:55:17 +0600

    third commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+  include 'admin/php_files/database.php';
+
+  
+  $hostname = "http://localhost/boi_pori";
+    
+?>
---
+++ instamojo.php
@@ -0,0 +1,94 @@
+<?php 
+include 'config.php';
+
+session_start();
+// $product_total=$_GET['product_total'];
+$user = $_SESSION['username'];
+
+
+
+$db = new Database();
+$db->select('options','site_name',null,null,null,null);
+$site_name = $db->getResult();
+
+
+// $db2=new Database();
+// $db2->select('products','',null,null,null,null);
+// $product_total=$db->getResult();
+
+///$ch = curl_init();
+
+///curl_setopt($ch, CURLOPT_URL, 'https://test.instamojo.com/api/1.1/payment-requests/');
+///curl_setopt($ch, CURLOPT_HEADER, FALSE);
+///curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+///curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+///curl_setopt($ch, CURLOPT_HTTPHEADER,
+            /// array("X-Api-Key:test_f95508c7ff3dc44e5240321ee4c",
+            ///       "X-Auth-Token:test_6a29ac898bb6e073597e3c1ed8a"));
+///$payload = Array(
+   /// 'purpose' => 'Payment to '.$site_name[0]['site_name'],
+   /// 'amount' => $_POST['product_total'],
+    // 'phone' => '',
+   /// 'buyer_name' => $user,
+   /// 'redirect_url' => $hostname.'/success.php',
+    // 'send_email' => true,
+    // 'webhook' => 'http://www.example.com/webhook/',
+    // 'send_sms' => true,
+    // 'email' => 'rainkapil@gmail.com',
+    ///'allow_repeated_payments' => false
+///);
+///curl_setopt($ch, CURLOPT_POST, true);
+///curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($payload));
+///$response = curl_exec($ch);
+///curl_close($ch);
+///$response = json_decode($response); 
+ //echo '<pre>';
+ //print_r($response);
+//exit;
+///$_SESSION['TID'] = $response->payment_request->id;
+$params1 = [
+    'item_number' => $_POST['product_id'],
+   /// 'txn_id' => $response->payment_request->id,
+    'payment_gross' => $_POST['product_total'],
+    'payment_status' => 'credit',
+];
+$params2 = [
+    'product_id' => $_POST['product_id'],
+    'product_qty' => $_POST['product_qty'],
+    'total_amount' => $_POST['product_total'],
+    'product_user' => $_SESSION['user_id'],
+    'order_date' => date('Y-m-d'),
+   /// 'pay_req_id' => $response->payment_request->id
+];
+
+$db = new Database();
+$db->insert('payments',$params1);
+$db->insert('order_products',$params2);
+$db->getResult();
+
+include 'header.php';
+
+///header('Location: '.$response->payment_request->longurl);
+?>
+<div >
+
+<h2><center>THANK YOU!</center></h2><br>
+<h4><center>Your order has been placed</center></h4>
+<p>
+<center>
+    Your user id : <?php echo $_SESSION['user_id']?><br>
+    Your Product id:  <?php echo $_POST['product_id']?><br>
+    Your Product quantity:  <?php echo $_POST['product_qty']?><br>
+    Total: <?php echo $_POST['product_total']?><br>
+    Order Date: <?php echo date('Y-m-d')?><br>
+    </center>
+
+</p>
+<p>
+    <center>
+<a class="btn btn-sm btn-primary remove-cart-item" href="index.php" data-id="<?php echo $row['product_id']; ?>"><i class="fa fa-add">Continue</i></a>
+</center>
+</p>
+
+</div>
+<?php include 'footer.php'; ?>
